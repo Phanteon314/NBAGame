79d21d15e1b04a49c4cde2076e1002776f0f68ef
phkx <phanteon314@gmail.com>
2024-04-18 14:58:39 +0200

    segundo commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/index.css')}}">
+    <script src="https://kit.fontawesome.com/c47bb574d3.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+   <header class="row">
+       @include('includes.header')
+   </header>
+   <div id="main" class="row">
+           @yield('content')
+   </div>
+   <footer class="row">
+       @include('includes.footer')
+   </footer>
+</div>
+</body>
+</html>
